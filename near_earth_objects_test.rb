7c63633a1be3a14d913f2a430439ac93f061c2c0
Jane Greene <janegreenecasa@gmail.com>
2020-06-24 17:11:31 -0600

    Added api key and new test for initialize

--- near_earth_objects_test.rb
+++ near_earth_objects_test.rb
@@ -8,4 +8,9 @@ class NearEarthObjectsTest < Minitest::Test
     results = NearEarthObjects.find_neos_by_date('2019-03-30')
     assert_equal '(2019 GD4)', results[:astroid_list][0][:name]
   end
+  
+  def test_it_exists
+   neos = NearEarthObjects.new('2019-03-30')
+   assert_instance_of NearEarthObjects, neos
+  end
 end
