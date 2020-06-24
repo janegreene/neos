b7bba1a3d97c8cf748df39d1cd6ffe06c6caac04
Jane Greene <janegreenecasa@gmail.com>
2020-06-24 17:59:02 -0600

    Added test for service that calls the API

--- near_earth_objects_test.rb
+++ near_earth_objects_test.rb
@@ -8,9 +8,16 @@ class NearEarthObjectsTest < Minitest::Test
     results = NearEarthObjects.find_neos_by_date('2019-03-30')
     assert_equal '(2019 GD4)', results[:astroid_list][0][:name]
   end
-  
-  def test_it_exists
-   neos = NearEarthObjects.new('2019-03-30')
-   assert_instance_of NearEarthObjects, neos
+
+  # def test_it_exists
+  #  neos = NearEarthObjects.new('2019-03-30')
+  #  assert_instance_of NearEarthObjects, neos
+  # end
+
+  def test_asteriods_list_data_service
+   results = NearEarthObjects.asteroids_list_data_service('2019-03-30')
+   assert_equal Faraday::Response, results.class
+   assert_equal String, results.body.class
   end
+
 end
